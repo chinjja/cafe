e5e17344e129288e36d833a2ced72ea9d3029152
chinjja <chinjja@gmail.com>
2021-06-13 03:23:54 +0900

    @SessionAttributes

--- src/main/java/com/chinjja/issue/web/HomeController.java
+++ src/main/java/com/chinjja/issue/web/HomeController.java
@@ -1,12 +1,17 @@
 package com.chinjja.issue.web;
 
+import javax.validation.Valid;
+
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.chinjja.issue.data.IssueRepository;
 import com.chinjja.issue.data.UserRepository;
@@ -20,6 +25,7 @@ import lombok.val;
 
 @Controller
 @RequiredArgsConstructor
+@SessionAttributes({"user", "issueList"})
 public class HomeController {
 	private final IssueRepository issueRepo;
 	private final UserRepository userRepo;
@@ -36,11 +42,15 @@ public class HomeController {
 	
 	@Secured("ROLE_USER")
 	@PostMapping("/new_issue")
-	public String createIssue(@AuthenticationPrincipal User user, IssueData data) {
+	public String createIssue(@AuthenticationPrincipal User user, @Valid IssueData data, Errors errors, SessionStatus status) {
+		if(errors.hasErrors()) {
+			return "home";
+		}
 		val issue = new Issue();
 		issue.setUser(user);
 		issue.setData(data);
 		issueRepo.save(issue);
+		status.setComplete();
 		return "redirect:/";
 	}
 	
