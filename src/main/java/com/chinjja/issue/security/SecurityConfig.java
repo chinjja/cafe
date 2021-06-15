fe8221e29bdc76a7023c7c5b47fad715c39af19c
chinjja <chinjja@gmail.com>
2021-06-15 19:43:15 +0900

    profile & dev profile visualization show all profile into badge except prod profile

--- src/main/java/com/chinjja/issue/security/SecurityConfig.java
+++ src/main/java/com/chinjja/issue/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.chinjja.issue.security;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		;
 	}
 	
+	@Profile("dev")
 	@Bean
 	public CommandLineRunner createUser() {
 		return args -> {
