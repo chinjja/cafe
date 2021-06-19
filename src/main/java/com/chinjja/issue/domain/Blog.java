6c8a03097049841bb5d5ca33f564c9828bb44271
chinjja <chinjja@gmail.com>
2021-06-20 02:25:44 +0900

    blog's title index for future search feature

--- src/main/java/com/chinjja/issue/domain/Blog.java
+++ src/main/java/com/chinjja/issue/domain/Blog.java
@@ -5,15 +5,20 @@ import java.util.List;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Entity
+@Table(indexes = {
+		@Index(columnList = "title"),
+		})
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Blog extends Element {
