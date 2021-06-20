e1a0964c5a7a80a3ec80c197b6c220d9595fd269
chinjja <chinjja@gmail.com>
2021-06-20 20:43:19 +0900

    국제화 기능 추가

--- src/main/java/com/chinjja/issue/web/WebConfig.java
+++ src/main/java/com/chinjja/issue/web/WebConfig.java
@@ -1,10 +1,19 @@
 package com.chinjja.issue.web;
 
+import java.util.Locale;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
+@EnableAutoConfiguration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
@@ -13,4 +22,22 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addViewController("/login");
 	}
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver resolver = new SessionLocaleResolver();
+		resolver.setDefaultLocale(Locale.KOREA);
+		return resolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		return interceptor;
+	}
 }
