17810635600811ed7a66f79a12a8e7c1e777e6a4
chinjja <chinjja@gmail.com>
2021-06-25 09:52:31 +0900

    spring.jpa.properties.hibernate.show_sql move to test

--- src/test/java/com/chinjja/issue/DataTests.java
+++ src/test/java/com/chinjja/issue/DataTests.java
@@ -1,14 +1,16 @@
 package com.chinjja.issue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@DataJpaTest(properties = {
+		"spring.jpa.properties.hibernate.show_sql=false",
+		"spring.jpa.properties.hibernate.format_sql=true"
+})
 @ActiveProfiles("test")
 class DataTests {
 	@Test
 	public void context() {
-		
 	}
 }
