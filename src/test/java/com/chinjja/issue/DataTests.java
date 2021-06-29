ea0bb37395f1ef1a1423f1c03fbd27fe0b3eb9ad
chinjja <chinjja@gmail.com>
2021-06-29 15:57:01 +0900

    user jpa test

--- src/test/java/com/chinjja/issue/DataTests.java
+++ src/test/java/com/chinjja/issue/DataTests.java
@@ -1,10 +1,7 @@
 package com.chinjja.issue;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,51 +12,16 @@ import org.springframework.test.context.ActiveProfiles;
 import com.chinjja.issue.domain.Cafe;
 import com.chinjja.issue.domain.Category;
 import com.chinjja.issue.domain.Category.Type;
-import com.chinjja.issue.form.CategoryForm;
-import com.chinjja.issue.form.CommentForm;
-import com.chinjja.issue.form.PostForm;
 import com.chinjja.issue.domain.Comment;
 import com.chinjja.issue.domain.Post;
 import com.chinjja.issue.domain.User;
-import com.chinjja.issue.domain.UserRole;
 
 import lombok.val;
-import lombok.var;
 
-@DataJpaTest(properties = {
-		"spring.jpa.properties.hibernate.show_sql=false",
-		"spring.jpa.properties.hibernate.format_sql=true"
-})
+@DataJpaTest
 @ActiveProfiles("test")
 class DataTests {
 	@Autowired TestEntityManager em;
-	
-	@Test
-	public void user() {
-		val user1 = User.builder()
-				.username("owner")
-				.build();
-		assertThrows(Exception.class, () -> em.persistAndFlush(user1));
-		em.clear();
-		
-		var user2 = User.builder()
-				.username("owner")
-				.password("1234")
-				.build();
-		user2 = em.persist(user2);
-		val cpy = user2.toBuilder().build();
-		assertEquals(user2, cpy);
-		assertFalse(user2.isAdmin());
-		
-		em.persist(UserRole.create(user2, "ROLE_ADMIN"));
-		em.flush();
-		em.clear();
-		user2 = em.find(User.class, user2.getId());
-		assertTrue(user2.isAdmin());
-		
-		val loadUser = em.find(User.class, user2.getId());
-		assertEquals(user2, loadUser);
-	}
 	
 	@Test
 	public void category() {
